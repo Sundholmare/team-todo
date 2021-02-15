8691b690b69de67ba44032073d95dd49d54ad210
Sundholmare <70331013+Sundholmare@users.noreply.github.com>
2021-02-15 16:24:45 +0100

    Clean

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
-      <h2>Hello from Frida</h2>
-      <h1>HEEEEEJ</h1>
-      <h1>Knott o mög, samir är nog hög</h1>
+      
     </div>
   );
 }
